a567d0813e020361c1f61913ba444b47cc3ca7b4
Sean Newman <sxn6296@rit.edu>
2020-03-22 23:45:29 -0400

    Enable mousewheel scrolling

--- keyboards/keebio/iris/keymaps/s-newman/config.h
+++ keyboards/keebio/iris/keymaps/s-newman/config.h
@@ -24,3 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_DELAY 100
 #define MOUSEKEY_MAX_SPEED 7
 #define MOUSEKEY_TIME_TO_MAX 15
+#define MOUSEKEY_WHEEL_DELAY 100
+#define MOUSEKEY_WHEEL_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 15
